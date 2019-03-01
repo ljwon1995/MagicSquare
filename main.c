aa15a2c00ab62e6e6105555d9702f8f88b11ed00
ljwon1995 <ljwon1995@naver.com>
2019-02-28 17:14:12 -0800

    Initial design

---
+++ main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#define MAX_SIZE 15
+
+void PrintSquare(){
+    
+}
+
+void FillSquare(){
+    
+}
+
+void InitSquare(){
+    
+}
+
+int IsAcceptable(){
+    
+}
+
+void GetSquareSizeByUser(){
+    IsAcceptable();
+}
+
+int main(){
+    int square[MAX_SIZE][MAX_SIZE];
+    
+    GetSquareSizeByUser();
+    InitSquare();
+    FillSquare();
+    PrintSquare();
+    
+    
+}
