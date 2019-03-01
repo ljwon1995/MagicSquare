c1a537ba3c78dd8b191f3582c85a0df16b458e87
ljwon1995 <ljwon1995@naver.com>
2019-02-28 17:25:05 -0800

    Finishing IsAcceptable and GetSize function

--- main.c
+++ main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 #define MAX_SIZE 15
 
 void PrintSquare(){
@@ -13,12 +15,44 @@ void InitSquare(){
     
 }
 
-int IsAcceptable(){
+int IsAcceptable(int size){ //return 1 to right, return -1 out of range, return -2 for even
+    
+    if(size > MAX_SIZE){
+        return -1;
+    }
+    
+    else if(size % 2 == 0){
+        return -2;
+    }
+    
+    else{
+        return size;
+    }
     
 }
 
-void GetSquareSizeByUser(){
-    IsAcceptable();
+int GetSquareSizeByUser(){
+    int size;
+    
+    printf("Enter the size of the square: ");
+    scanf("%d", &size);
+    switch(IsAcceptable(size)){
+        case 1:
+            return size;
+            break;
+        case -1:
+            fprintf(stderr, "ERROR! Size is out of range\n");
+            exit(EXIT_FAILURE);
+            break;
+        case -2:
+            fprintf(stderr, "ERROR! Size is even\n");
+            exit(EXIT_FAILURE);
+            break;
+            
+    }
+    fprintf(stderr, "ERROR! Abnormal exit in GetSquareSizeByUser()\n");
+    exit(EXIT_FAILURE);
+    
 }
 
 int main(){
